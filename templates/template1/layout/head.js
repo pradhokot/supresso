5bb41edb574d25b8d6875b1099a02d478d126a9d
pradhokot <yoggi60@gmail.com>
2025-06-25 14:24:14 +0700

    2025 june 25, update 2

---
+++ templates/template1/layout/head.js
@@ -0,0 +1,12 @@
+document.writeln(`
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="../../assets/img/logo-supresso.ico" type="image/x-icon">
+<link rel="stylesheet" href="../../assets/vendor/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../assets/vendor/fontawesome-free-6.7.2-web/css/all.min.css">
+<link rel="stylesheet" href="../../assets/font/Gotham/HelveticaNeue.css">
+<link rel="stylesheet" href="../../assets/font/Gotham/HelveticaNeue.css">
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/global.css">
+<script src="../../assets/vendor/jquery-3.7.1.min.js.js"></script>
+<script src="../../assets/vendor/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>`)
