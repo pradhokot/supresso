f293e97950e89a66d53e6726bd939acbe138ec85
pradhokot <yoggi60@gmail.com>
2025-06-25 13:59:11 +0700

    first commit

---
+++ templates/template1/js/global.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+   // Set tinggi minimum page-body dan padding page
+   // var windowHeight = $(window).height();
+   // var headerHeight = $('body > header').outerHeight();
+   // $('body').css('padding-top', headerHeight + 'px');
+
+   // Responsive base on screen width
+   if ($(window).width() >= 992) { } else { }
+
+});
---
+++ templates/template1/layout/footbar.js
@@ -0,0 +1,179 @@
+document.writeln(`
+<footer class="footbar">
+   <hr class="opacity-100">
+   <div class="container d-flex flex-column row-gap-4 py-4">
+      <ul class="nav-footbar list-unstyled text-uppercase d-flex flex-wrap justify-content-center gap-2">
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex" id="nav-home">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     home
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     beans
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     ground
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     drip
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     capsules
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     bundles
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     partnership
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     membership
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="text-reset text-decoration-none position-relative px-2 d-flex">
+               <span class="link-text link-hover">
+                  <b class="fw-medium">
+                     brochure
+                  </b>
+               </span>
+               <span class="link-icon position-absolute top-100 start-50 translate-middle mt-1">&bull;</span>
+            </a>
+         </li>
+      </ul><!-- end nav footbar -->
+      <nav class="text-center nav-footbar-sosmed d-flex justify-content-center align-items-center gap-3 column-gap-lg-4">
+         <a href="#" target="_blank" class="text-decoration-none text-reset link-hover d-flex justify-content-center align-items-center position-relative">
+            <span class="link-text d-block w-100 h-100 rounded-circle border border-2 border-reset position-absolute top-50 start-50"></span>
+            <span class="link-icon position-absolute top-50 start-50">
+               <i class="fab fa-x-twitter"></i>
+            </span>
+         </a>
+         <a href="#" target="_blank" class="text-decoration-none text-reset link-hover d-flex justify-content-center align-items-center position-relative">
+            <span class="link-text d-block w-100 h-100 rounded-circle border border-2 border-reset position-absolute top-50 start-50"></span>
+            <span class="link-icon position-absolute top-50 start-50">
+               <i class="fab fa-facebook-f"></i>
+            </span>
+         </a>
+         <a href="#" target="_blank" class="text-decoration-none text-reset link-hover d-flex justify-content-center align-items-center position-relative">
+            <span class="link-text d-block w-100 h-100 rounded-circle border border-2 border-reset position-absolute top-50 start-50"></span>
+            <span class="link-icon position-absolute top-50 start-50">
+               <i class="fab fa-instagram"></i>
+            </span>
+         </a>
+         <a href="#" target="_blank" class="text-decoration-none text-reset link-hover d-flex justify-content-center align-items-center position-relative">
+            <span class="link-text d-block w-100 h-100 rounded-circle border border-2 border-reset position-absolute top-50 start-50"></span>
+            <span class="link-icon position-absolute top-50 start-50">
+               <i class="fab fa-youtube"></i>
+            </span>
+         </a>
+         <a href="#" target="_blank" class="text-decoration-none text-reset link-hover d-flex justify-content-center align-items-center position-relative">
+            <span class="link-text d-block w-100 h-100 rounded-circle border border-2 border-reset position-absolute top-50 start-50"></span>
+            <span class="link-icon position-absolute top-50 start-50">
+               <i class="fab fa-tiktok"></i>
+            </span>
+         </a>
+         <a href="#" target="_blank" class="text-decoration-none text-reset link-hover d-flex justify-content-center align-items-center position-relative">
+            <span class="link-text d-block w-100 h-100 rounded-circle border border-2 border-reset position-absolute top-50 start-50"></span>
+            <span class="link-icon position-absolute top-50 start-50">
+               <i class="fab fa-linkedin-in"></i>
+            </span>
+         </a>
+      </nav>
+   </div><!-- end container -->
+   <hr>
+   <div class="container small">
+      <div class="row align-items-lg-baseline pb-4">
+         <div class="col col-12 col-lg-auto">
+            <select name="" class="form-select form-select-sm rounded-0 mb-3 w-auto border-reset mx-auto" id="">
+               <option value="1" selected>ENGLISH</option>
+               <option value="2">INDONESIA</option>
+            </select>
+         </div><!-- end col -->
+         <div class="col col-12 col-lg">
+            <ul class="list-unstyled nav-footbar-privacy text-capitalize d-flex flex-wrap justify-content-center row-gap-2 justify-content-lg-start">
+               <li>
+                  <a href="#" target="_blank" class="text-reset link-hover link-hover-underline d-grid">
+                     <span class="link-text">privacy policy</span>
+                  </a>
+               </li>
+               <li><div class="vr h-100 mx-2"></div></li>
+               <li>
+                  <a href="#" target="_blank" class="text-reset link-hover link-hover-underline d-grid">
+                     <span class="link-text">terms & conditions</span>
+                  </a>
+               </li>
+               <li><div class="vr h-100 mx-2"></div></li>
+               <li>
+                  <a href="#" target="_blank" class="text-reset link-hover link-hover-underline d-grid">
+                     <span class="link-text">information on data protection</span>
+                  </a>
+               </li>
+               <li><div class="vr h-100 mx-2"></div></li>
+               <li>
+                  <a href="#" target="_blank" class="text-reset link-hover link-hover-underline d-grid">
+                     <span class="link-text">help?</span>
+                  </a>
+               </li>
+            </ul>
+         </div><!-- end col -->
+         <div class="col col-12 col-lg-auto">
+            <p class="text-center text-lg-start">
+               &copy; 2021 - <script>document.write(new Date().getFullYear())</script> SUPRESSO. All rights reserved
+            </p>
+         </div><!-- end col -->
+      </div><!-- end row -->
+   </div><!-- end container -->
+</footer>`)
